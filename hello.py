0be51360a43c33347a48423cad33deced326e57d
Travis Dean <travis.j.dean@gmail.com>
2014-09-29 15:00:01 -0400

    Added random file to do random things.

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 # Travis Dean - tjd2qj
 from helper import greeting
+from random import do_something_weird
 
 def run():
     greeting("Hello")
-
-
+    do_something_weird()
 
 
 if __name__=="__main__":
---
+++ random.py
@@ -0,0 +1,7 @@
+# Travis Dean - tjd2qj
+
+def do_something_weird():
+    print("WEIRD THINGS")
+    print("WHY AM I STUCK IN A COMPUTER")
+
+
